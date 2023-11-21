5b31974e862a1d8ebc2f19c111eadf28175e8d64
Nicolas Caldas <nicolascaldas10@gmail.com>
2023-11-21 18:07:54 -0300

    added footer and index controller/model/router

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_155218) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_193654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accessories", force: :cascade do |t|
+    t.string "location"
+    t.float "price"
+    t.boolean "availability"
+    t.text "description"
+    t.string "condition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
