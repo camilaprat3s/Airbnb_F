2d94a7bb91068618a7585fd3f1dc0610acebbb6f
camilaprates <camilaprates1@gmail.com>
2023-11-21 13:39:19 -0600

    First and Last Name

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < ApplicationController
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :first_name, :last_name)
+  end
+end
