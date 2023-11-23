a5a95540bfba85b49cb413e816168d778d90d7c5
Nicolas Caldas <nicolascaldas10@gmail.com>
2023-11-23 18:43:08 -0300

    added cards/second navbar. changed bootstrap colors in _bootstrap_variables.scss and _colors.scss. added image_url migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,14 @@ puts "Cleaning database..."
 Accessory.destroy_all
 
 puts "Creating Accessories..."
-watch = {name: "Lunar Watch", location: "moon", price: 400000.124, availability: true, description: "very nice silver watch with a small lunar stone", condition: "meh"}
-bracelet = {name: "Helios' Bracelet", location: "sun", price: 19999999999999, availability: false, description: "this bracelet makes you very powerful", condition: "perfect"}
+watch = {name: "Lunar Watch", location: "moon", price: 400000.124, availability: true, description: "very nice silver watch with a small lunar stone", condition: "meh", image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg"}
+bracelet = {name: "Helios' Bracelet", location: "sun", price: 19999999999999, availability: false, description: "this bracelet makes you very powerful", condition: "perfect", image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/greece.jpg"}
 
 [watch, bracelet].each do |attributes|
   accessory = Accessory.create!(attributes)
   puts "Created #{accessory.description}"
 end
 puts "Finished!"
+
+# ../../assets/images/moon_watch.jpg
+# ../../assets/images/solar_bracelet.jpg
