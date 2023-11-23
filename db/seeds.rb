34f53be8bae00bfabbce368d05db820d8e7c96cc
Matt Trusson <matt.trusson@hotmail.com>
2023-11-23 17:06:37 -0300

    frontend view added for SHOW. Image and category added to model

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,8 @@ Accessory.destroy_all
 user= User.create(email: "user@mail.com", password: "1234567", first_name: "Name", last_name: "Last")
 
 puts "Creating Accessories..."
-watch = {name: "Lunar Watch", location: "moon", price_per_day: 400000.124, availability: true, description: "very nice silver watch with a small lunar stone", condition: "meh", category: "jewellery"}
-bracelet = {name: "Helios' Bracelet", location: "sun", price_per_day: 19999999999999, availability: false, description: "this bracelet makes you very powerful", condition: "perfect", category: "jewellery"}
-
+watch = {name: "Lunar Watch", location: "moon", price_per_day: 400000.124, availability: true, description: "very nice silver watch with a small lunar stone", condition: "meh", category: "jewellery", image: "https://www.louismoinet.com/wp-content/uploads/Produits/Moon/Moon_LM-45-50-MO.jpg"}
+bracelet = {name: "Helios' Bracelet", location: "sun", price_per_day: 19999999999999, availability: false, description: "this bracelet makes you very powerful", condition: "perfect", category: "jewellery", image: "https://www.rebelandrose.com/media/images/rrproducts/png/big/RR-BR035-S.png?_=1695282617"}
 
 [watch, bracelet].each do |attributes|
   accessory = Accessory.create!(attributes)
